b557ce7c6267642d0ec2ab4d4332527522fcf6e2
Mike <michael.hallam3@gmail.com>
2018-04-07 09:16:18 +0100

    Listen code
    I believe it waits and listens for incoming communications on port 8080. No way of knowing though, because I've no idea how to send information...

--- transmissionTest/src/transmissiontest/TransmissionTest.java
+++ transmissionTest/src/transmissiontest/TransmissionTest.java
@@ -9,13 +9,85 @@ package transmissiontest;
  *
  * @author p2titus
  */
-public class TransmissionTest {
-
+import java.io.*;
+import java.net.*;
+public class TransmissionTest
+{
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+        BufferedReader BR = null;
+        InputStreamReader ISR = null;
+        int port = 8080;
+        ServerSocket listener = null;
+        Socket s = null;
+        String inputLine = "";
+        short counter = 0;
+        
+        try
+        {
+            System.out.println("FLAG 1");
+            listener = new ServerSocket(port);
+            System.out.println("FLAG 2");
+        }
+        
+        catch(final IOException ex)
+        {
+            System.out.println("IOEXCEPTION 1");
+        }
+        
+        try
+        {
+            System.out.println("FLAG 3");
+            try
+            {
+                System.out.println("FLAG 4");
+                s = listener.accept(); // halting on this line, probably because nothing is sending data...
+                System.out.println("FLAG 5");
+                ISR = new InputStreamReader(s.getInputStream());
+                BR = new BufferedReader(ISR);
+                while((inputLine = BR.readLine()) != null)
+                {
+                    counter++;
+                    System.out.println(inputLine);
+                    if(counter > 1000)
+                        break;
+                }
+            }
+            
+            catch(final IOException ex)
+            {
+                System.out.println("IOEXCEPTION 2");
+            }
+            
+            finally
+            {
+                try
+                {
+                    s.close();
+                }
+                
+                catch(final IOException ex)
+                {
+                    System.out.println("IOEXCEPTION 3");
+                }
+            }
+        }
+        
+        finally
+        {
+            try
+            {
+                listener.close();
+            }
+            
+            catch(final IOException ex)
+            {
+                System.out.println("IOEXCEPTION 1");
+            }
+        }
     }
     
 }
