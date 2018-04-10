22689a41e7aaaced5d108ff14cbe4e710d6b8689
Mike <michael.hallam3@gmail.com>
2018-04-10 20:42:16 +0100

    Comments
     - comments added
     - specific libraries added to allow for faster execution
     - small refactors

--- transmissionTest/src/transmissiontest/TransmissionTest.java
+++ transmissionTest/src/transmissiontest/TransmissionTest.java
@@ -5,14 +5,15 @@
  */
 package transmissiontest;
 
-
-
 /**
  *
  * @author p2titus
  */
-import java.io.*;
-import java.net.*;
+
+import java.io.IOException; // used to catch errors that could occur
+import java.io.PrintWriter; // used to send data over the socket object
+import java.net.Socket; // used to establish a connection between client and server
+
 public class TransmissionTest
 {
 
@@ -21,29 +22,41 @@ public class TransmissionTest
      */
     public static void main(String[] args)
     {
+        
+        /*
+        ** exceptions are numbered allowing exception triggered to be easily identified
+        ** subclass receivingData receives data sent in the main()
+        ** runs as a separate thread to allow for data to be sent and received in same program (and also cos it's cool)
+        */
+        
         final TransmissionTest gm = new TransmissionTest(); // gm = getMethod
-        final receivingData rd;
+        final receivingData rd; // rd = receivingData
         
-        final int port = 8080;
-        final String ipToCon = "127.0.0.1";
-        String msg = "Failure";
+        final int port = 8080; // port to be used for communications. Left as a default of 8080, but users can change this
+        // since this program wasn't intended to be more than sending data using sockets, I'm leaving it on 8080
+        // all parts of this program will use this port variable, so feel free to change it
+        final String ipToCon = "127.0.0.1"; // home ip address. probably possible to connect to external ip address, but I haven't tested yet
+        String msg = "Failure"; // variable storing message. If program has failed in any way, failure will be output
         
-        rd = new receivingData(port); // rd = receivingData
-        System.out.println("FLAG 1");
-        rd.start(); // program hangs here
+        rd = new receivingData(port); // passing port variable to class, so it knows which port to be listening on
+        System.out.println("FLAG 1"); // flags are present, allowing me to easily see where the program may be hanging
+        rd.start(); // program hangs here - fixed (issue was using run() instead of start()
+        // starts the code written under the overridden run() method in a new thread
+        // as this is running in a separate thread, it will be able to receive the data at the same time
         System.out.println("FLAG 2");
-        gm.sendMsg("Hello There!", ipToCon, port);
-        msg = rd.msg();
+        gm.sendMsg("Hello There!", ipToCon, port); // sending the message Hello There!
+        msg = rd.msg(); // storing the received message in the msg variable
         System.out.println("FLAG 3");
         System.out.println(msg); // outputting correct response, but only some of the time?
+        // probably because sending the message and receiving it has to take place in the time it takes to execute one line of code
     }
     
-    private void sendMsg(String msg, String ip, int port)
+    private void sendMsg(String msg, String ip, int port) // prepares data to be sent by method sendData
     {
         try
         {
             //Socket s = new Socket(ip, port);
-            sendData(msg, new Socket(ip, port));
+            sendData(msg, new Socket(ip, port)); // passes onto next method with socket object instantiated
         }
         
         catch(final IOException ex)
@@ -53,20 +66,32 @@ public class TransmissionTest
         
     }
     
-    private void sendData(String msg, Socket sendData)
+    private void sendData(String msg, Socket sendData) // method that sends data over a network
     {
-        PrintWriter PW = null;
+        PrintWriter PW = null; // will send message over network
         
         try
         {
-            PW = new PrintWriter(sendData.getOutputStream());
-            PW.println(msg);
-            PW.close();
+            PW = new PrintWriter(sendData.getOutputStream()); // instantiating object with socket output stream
+            // allows for data to be sent via the socket object
+            PW.println(msg); // sending the message
+            // it could be adapted using an arraylist to send more data
+            PW.close(); // closes printwriter object
         }
         
         catch(final IOException ex)
         {
             System.out.println("IOEXCEPTION 2");
+        }
+        
+        try
+        {
+            sendData.close(); // closes socket object, closing the connection between client and server
+        }
+        
+        catch(final IOException ex)
+        {
+            System.out.println("IOEXCEPTION 3");
         }
     }
 }
--- transmissionTest/src/transmissiontest/receivingData.java
+++ transmissionTest/src/transmissiontest/receivingData.java
@@ -9,22 +9,28 @@ package transmissiontest;
  *
  * @author p2titus
  */
-import java.net.*;
-import java.io.*;
-public class receivingData extends Thread
+
+import java.io.BufferedReader; // allows for message being received to be read
+import java.io.IOException; // allows for ioexceptions to be caught and dealt with
+import java.io.InputStreamReader; // allows for the input stream to be read
+import java.net.Socket; // allows for a connection to be established to the client
+import java.net.ServerSocket; // allows for a connection to be established to the client
+
+public class receivingData extends Thread // allows for threads to be used in this class
 {
-    private String msg = null;
-    private int port = 8080;
+    private String msg = null; // storing the message that will be received
+    private int port = 8080; // storing the port used - default same as main class
     
-    public receivingData(int port)
+    public receivingData(int port) // allows user to pass in their own custom port
     {
-        this.port = port;
+        this.port = port; // assigns user's port value to value of port in class
     }
     
     @Override
-    public void run()
+    public void run() // all code that will be executed in a separate thread to the main
     {
-        msg = receiveData();
+        msg = receiveData(); // variable msg will hold the message
+        // as run has a void return type, this was the simplest workaround
     }
     
     private String receiveData()
@@ -33,7 +39,7 @@ public class receivingData extends Thread
         //ServerSocket listener = null;
         try
         {
-            msgToRet = recvData(new ServerSocket(port));
+            msgToRet = recvData(new ServerSocket(port)); // passing on server object to receiveData method
         }
         
         catch(final IOException ex)
@@ -41,23 +47,27 @@ public class receivingData extends Thread
             System.out.println("IOEXCEPTION A1");
         }
         
-        return msgToRet;
+        return msgToRet; // returns the message that was received by the server
     }
     
-    private String recvData(ServerSocket listener)
+    private String recvData(ServerSocket listener) // code that will receive the message sent by the user
     {
-        String msgToRet = null;
-        Socket s = null;
+        String msgToRet = null; // the message to be returned
+        Socket s = null; // socket that 
         InputStreamReader ISR = null;
         BufferedReader BR = null;
         
         try
         {
-            while(true)
+            /*while(true)
             {
-                s = listener.accept(); // accepting incoming connections
-                break;
-            }
+                s = listener.accept(); // accepts any incoming connections
+                break; // breaks out of loop when accepted
+                // as accept() will hang unti a connection is found, this loop will end when a connection is found
+            }*/
+            
+            s = listener.accept(); // accepts any incoming connections
+            // thread will hang until connection found
         }
         
         catch(final IOException ex)
@@ -68,8 +78,8 @@ public class receivingData extends Thread
         try
         {
             ISR = new InputStreamReader(s.getInputStream());
-            BR = new BufferedReader(ISR);
-            msgToRet = BR.readLine();
+            BR = new BufferedReader(ISR); // allows for data sent to be read
+            msgToRet = BR.readLine(); // msg sent stored in variable
         }
         
         catch(final IOException ex)
@@ -79,7 +89,7 @@ public class receivingData extends Thread
         
         finally
         {
-            try
+            try // closing all objects
             {
                 BR.close();
                 ISR.close();
@@ -93,10 +103,10 @@ public class receivingData extends Thread
             }
         }
         
-        return msgToRet;
+        return msgToRet; // returns sent message
     }
         
-    public String msg()
+    public String msg() // public interface - returns the message sent
     {
         return msg;
     }
